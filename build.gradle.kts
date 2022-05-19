a4353f470dc407f62ccc1e304851b8250e1df35c
diegoj <oliveiraj.diego@gmail.com>
2022-05-19 11:02:00 -0300

    feat: add swagger documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.flywaydb:flyway-core:8.5.8")
 	implementation("org.flywaydb:flyway-mysql:8.5.8")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 
 	runtimeOnly( "mysql:mysql-connector-java")
 
